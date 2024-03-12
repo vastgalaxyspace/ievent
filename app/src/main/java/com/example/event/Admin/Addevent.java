8d46af68316d429c7ffa82390f44fd35332fcc27
student <dhirajchavan355@gmail.com>
2024-03-12 08:15:13 +0530

    fixed error

--- app/src/main/java/com/example/event/Admin/Addevent.java
+++ app/src/main/java/com/example/event/Admin/Addevent.java
@@ -1,6 +1,9 @@
 package com.example.event.Admin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.event.R;
 
 public class Addevent extends AppCompatActivity {
+    private static final int PICK_IMAGE=1;
+    private ImageView eventadd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,16 @@ public class Addevent extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        eventadd=findViewById(R.id.eventImage);
+        eventadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(intent);
+
+            }
         });
     }
 }
--- app/src/main/java/com/example/event/Adminactivity.java
+++ app/src/main/java/com/example/event/Adminactivity.java
@@ -1,14 +1,21 @@
 package com.example.event;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.event.Admin.Addevent;
+
 public class Adminactivity extends AppCompatActivity {
+
+    private CardView  addevent ,registerstu,vieweve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,20 @@ public class Adminactivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        addevent = findViewById(R.id.addeventcard);
+        registerstu=findViewById(R.id.registerstudentcard); // corrected line
+        vieweve=findViewById(R.id.seeevent);
+
+        addevent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Adminactivity.this, Addevent.class);
+                startActivity(intent);
+                finish();
+
+            }
         });
     }
 }
